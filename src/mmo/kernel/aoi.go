44aa8860ed53c556fffd8423b01bbce5fa21ed2f
vick_glb <vick_glb@me.com>
2019-12-05 15:55:55 +0800

    MMO AOIManager add GetSurroundingsByGid function

--- src/mmo/kernel/aoi.go
+++ src/mmo/kernel/aoi.go
@@ -57,6 +57,48 @@ func (am *AOIManager) gridHeight() int {
 	return (am.Bottom - am.Top) / am.GridsY
 }
 
+//根据gid 返回当前Grid 周围环境（九宫格）集合
+func (am *AOIManager) GetSurroundingsByGid(gid int) (grids []*Grid) {
+	//判断gid 是否存在
+	if _, ok := am.GridMap[gid]; !ok {
+		return nil
+	}
+	//将当前格子 加入九宫格集合
+	grids = append(grids, am.GridMap[gid])
+
+	//当前格式在X轴上的序号（从0开始）
+	gX := gid % am.GridsX
+	//判断左侧是否有格子，如果有，加入集合
+	if gX > 0 {
+		grids = append(grids, am.GridMap[gid-1])
+	}
+	//判断右侧是否有格子，如果有，加入集合
+	if gX < am.GridsX-1 {
+		grids = append(grids, am.GridMap[gid+1])
+	}
+
+	//将X轴方向所有格子 入Xgrids集合中
+	Xgrids := make([]int, 0, len(grids))
+	for _, v := range grids {
+		Xgrids = append(Xgrids, v.GID)
+	}
+
+	//遍历X轴格式，判断每个格子的上下是否有格式，如果有加入集合
+	for _, v := range Xgrids {
+		//当前格式在Y轴上的序号（从0开始）
+		gY := gid / am.GridsX
+		//判断上方是否有格式
+		if gY > 0 {
+			grids = append(grids, am.GridMap[v-am.GridsX])
+		}
+		//判断下方是否有格子
+		if gY < am.GridsY-1 {
+			grids = append(grids, am.GridMap[v+am.GridsX])
+		}
+	}
+	return
+}
+
 // 重写 String()
 func (am *AOIManager) String() string {
 	s := fmt.Sprintf("AOIManage:\nLeft:%d,Right:%d,GridsX:%d,Top:%d,Bottom:%d,GridsY:%d\n",
--- src/mmo/kernel/aoi_test.go
+++ src/mmo/kernel/aoi_test.go
@@ -7,7 +7,24 @@ import (
 
 func TestAOIManager(t *testing.T) {
 	//初始化AOIManager
-	am := NewAOIManager(100, 300, 4, 200, 350, 5)
+	am := NewAOIManager(100, 300, 4, 200, 400, 4)
 	//打印
 	fmt.Println(am)
+}
+
+func TestGetSurroundingsByGid(t *testing.T) {
+	//初始化AOIManager
+	am := NewAOIManager(100, 300, 4, 200, 400, 4)
+
+	for gid, _ := range am.GridMap {
+
+		grids := am.GetSurroundingsByGid(gid)
+		fmt.Println("gid:", gid, "grid Len", len(grids))
+		allGid := make([]int, 0, len(grids))
+		for _, grid := range grids {
+			allGid = append(allGid, grid.GID)
+		}
+		fmt.Println("周围grid有：", allGid)
+
+	}
 }
